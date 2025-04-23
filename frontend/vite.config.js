bd045a9550179dc60c63c2a280cd521136e28d81
ivan2330 <ivankozhevnyk@gmail.com>
2025-04-23 03:29:44 +0300

    Updating configs

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,27 +1,23 @@
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 
-// ✅ прочитати значення з process.env
-const API_URL = process.env.VITE_API_URL || '';
-const WS_URL = process.env.VITE_WS_URL || '';
-
 export default defineConfig({
   plugins: [react()],
   define: {
-    'import.meta.env.VITE_API_URL': JSON.stringify(API_URL),
-    'import.meta.env.VITE_WS_URL': JSON.stringify(WS_URL)
+    'import.meta.env.VITE_API_URL': JSON.stringify(process.env.VITE_API_URL || ''),
+    'import.meta.env.VITE_WS_URL': JSON.stringify(process.env.VITE_WS_URL || '')
   },
   server: {
     host: true,
     proxy: {
       '/auth': {
-        target: API_URL,
+        target: process.env.VITE_API_URL || '',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/auth/, '/auth'),
         secure: false,
       },
       '/users': {
-        target: API_URL,
+        target: process.env.VITE_API_URL || '',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/users/, '/users'),
         secure: false,
