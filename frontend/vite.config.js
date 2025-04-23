ff8b73b3055e76fabf82b7554685a5e36f22fd0f
ivan2330 <ivankozhevnyk@gmail.com>
2025-04-23 03:35:07 +0300

    Updating configs2

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,23 +1,31 @@
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 
+// Підтримка змінних оточення для білду
+const API_URL = process.env.VITE_API_URL || '';
+const WS_URL = process.env.VITE_WS_URL || '';
+
 export default defineConfig({
   plugins: [react()],
   define: {
-    'import.meta.env.VITE_API_URL': JSON.stringify(process.env.VITE_API_URL || ''),
-    'import.meta.env.VITE_WS_URL': JSON.stringify(process.env.VITE_WS_URL || '')
+    'import.meta.env.VITE_API_URL': JSON.stringify(API_URL),
+    'import.meta.env.VITE_WS_URL': JSON.stringify(WS_URL),
+  },
+  build: {
+    target: 'esnext', // потрібне для import.meta
+    outDir: 'dist',
   },
   server: {
     host: true,
     proxy: {
       '/auth': {
-        target: process.env.VITE_API_URL || '',
+        target: API_URL,
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/auth/, '/auth'),
         secure: false,
       },
       '/users': {
-        target: process.env.VITE_API_URL || '',
+        target: API_URL,
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/users/, '/users'),
         secure: false,
